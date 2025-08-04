0e4417b76c0b4008eda9edb6849fc5b9e6acc000
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-08-04 02:16:30 -0600

    add a new blog post

--- src/lib/blog/index.js
+++ src/lib/blog/index.js
@@ -1,9 +1,11 @@
-import p3_6_2024 from './md/3-6-2024.md?raw';
-import p3_7_2024 from './md/3-7-2024.md?raw';
-import p8_28_2024 from './md/8-28-2024.md?raw';
+import p8_4_2025 from './md/8-4-2025.md?raw';
 import p7_24_2025 from './md/7-24-2025.md?raw';
+import p8_28_2024 from './md/8-28-2024.md?raw';
+import p3_7_2024 from './md/3-7-2024.md?raw';
+import p3_6_2024 from './md/3-6-2024.md?raw';
 
 export default [
+    p8_4_2025,
     p7_24_2025,
     p8_28_2024,
     p3_7_2024,
