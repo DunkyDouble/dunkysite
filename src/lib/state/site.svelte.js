b306e002e1563b969e5126dd1d9e9f0ca042dbf8
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-10-20 03:40:45 -0600

    exit beta, pretty much everything i want is here

--- src/lib/state/site.svelte.js
+++ src/lib/state/site.svelte.js
@@ -1,6 +1,6 @@
 const SiteState = $state({
     // Whether or not the site is in BETA mode
-    beta: true,
+    beta: false,
 
     // The list of games on the front page (dependent on GamesComponent)
     // NOTE: This is also the apps list if we are told to use it
